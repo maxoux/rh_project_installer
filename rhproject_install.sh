bc80cfe29de1756318636fa334ef0f63558d43dc
Matthieu Laizé <matthieu@laize.pro>
2016-10-18 17:13:56 +0200

    add version 6.1.0 of node

--- rhproject_install.sh
+++ rhproject_install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 PRIMARY_BRANCH="refonte_back"
-DIRNAME="back"
+DIRNAME="rhproject_back"
+NODE_VERSION="6.1.0"
 
 echo "Clone distant repository"
 git clone git@bitbucket.org:darkael/rhproject.git ${DIRNAME}
@@ -9,6 +10,7 @@ cd ${DIRNAME}
 echo "Switch to right branch"
 git checkout ${PRIMARY_BRANCH}
 git pull
+sudo n ${NODE_VERSION}
 echo "Execute pre-install script"
 sudo node ./scripts/preinstall.js
 echo "Install dependencies"
